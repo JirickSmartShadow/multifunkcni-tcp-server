93510fd1d01c7ae445922b712ea622fbe8e1043c
Jiří Laštovka <98921568+JirickSmartShadow@users.noreply.github.com>
2023-04-17 19:32:56 +0200

    Add port configuration

--- ZaverecnyProjekt-TcpServer/sources/Program.cs
+++ ZaverecnyProjekt-TcpServer/sources/Program.cs
@@ -1,11 +1,12 @@
-﻿namespace ZaverecnyProjekt_TcpServer
+﻿using System.Configuration;
+
+namespace ZaverecnyProjekt_TcpServer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            TcpServer server = new TcpServer(65525);
+            TcpServer server = new TcpServer(int.Parse(ConfigurationManager.AppSettings["port"]));
         }  
     }
 }
-
